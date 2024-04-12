6c7430d1ec8dfd6e2b8aed344371779004299a8c
leejg <jeaguen@naver.com>
2024-04-12 13:45:28 +0900

    삭제처리

--- dss-publish/src/main.ts
+++ dss-publish/src/main.ts
@@ -3,69 +3,58 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import { router } from './router';
 import vuetify from './plugins/vuetify';
-
 import '@/scss/style.scss';
 import '@/assets/dss/scss/style.scss';
-import PerfectScrollbar from 'vue3-perfect-scrollbar';
-import VueApexCharts from 'vue3-apexcharts';
-import VueTablerIcons from 'vue-tabler-icons';
-import Vue3Marquee from 'vue3-marquee';
-
-import SvgSprite from '@/components/shared/SvgSprite.vue';
-
+// import PerfectScrollbar from 'vue3-perfect-scrollbar';
+// import VueApexCharts from 'vue3-apexcharts';
+// import VueTablerIcons from 'vue-tabler-icons';
+// import Vue3Marquee from 'vue3-marquee';
+// import SvgSprite from '@/components/shared/SvgSprite.vue';
 // google-fonts
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/700.css';
-
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
-
-import '@fontsource/poppins/400.css';
-import '@fontsource/poppins/500.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/700.css';
-
-import '@fontsource/public-sans/400.css';
-import '@fontsource/public-sans/500.css';
-import '@fontsource/public-sans/600.css';
-import '@fontsource/public-sans/700.css';
-
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/700.css';
+// import '@fontsource/inter/400.css';
+// import '@fontsource/inter/500.css';
+// import '@fontsource/inter/600.css';
+// import '@fontsource/inter/700.css';
+// import '@fontsource/poppins/400.css';
+// import '@fontsource/poppins/500.css';
+// import '@fontsource/poppins/600.css';
+// import '@fontsource/poppins/700.css';
+// import '@fontsource/public-sans/400.css';
+// import '@fontsource/public-sans/500.css';
+// import '@fontsource/public-sans/600.css';
+// import '@fontsource/public-sans/700.css';
 //Mock Api data
-import './_mockApis';
-import { fakeBackend } from '@/utils/helpers/fake-backend';
-
-import { vMaska } from 'maska';
+// import './_mockApis';
+// import { fakeBackend } from '@/utils/helpers/fake-backend';
+// import { vMaska } from 'maska';
 // print
 import print from 'vue3-print-nb';
 // Table
-import Vue3EasyDataTable from 'vue3-easy-data-table';
+// import Vue3EasyDataTable from 'vue3-easy-data-table';
 //i18
-import { createI18n } from 'vue-i18n';
-import messages from '@/utils/locales/messages';
-
-const i18n = createI18n({
-  locale: 'en',
-  messages: messages,
-  silentTranslationWarn: true,
-  silentFallbackWarn: true
-});
-
+// import { createI18n } from 'vue-i18n';
+// import messages from '@/utils/locales/messages';
+// const i18n = createI18n({
+//   locale: 'en',
+//   messages: messages,
+//   silentTranslationWarn: true,
+//   silentFallbackWarn: true
+// });
 const app = createApp(App);
-fakeBackend();
-
+// fakeBackend();
 app.use(router);
-app.component('EasyDataTable', Vue3EasyDataTable);
-app.use(PerfectScrollbar);
-app.component('SvgSprite', SvgSprite);
+// app.component('EasyDataTable', Vue3EasyDataTable);
+// app.use(PerfectScrollbar);
+// app.component('SvgSprite', SvgSprite);
 app.use(createPinia());
-app.use(VueTablerIcons);
+// app.use(VueTablerIcons);
 app.use(print);
-app.use(Vue3Marquee);
-app.use(i18n);
-app.directive('maska', vMaska);
-app.use(VueApexCharts);
+// app.use(Vue3Marquee);
+// app.use(i18n);
+// app.directive('maska', vMaska);
+// app.use(VueApexCharts);
 app.use(vuetify).mount('#app');
