9e1ea2a379ff90976e913b78111c35b563b9e573
leejg <jeaguen@naver.com>
2024-04-17 18:12:26 +0900

    메인 추가

--- dss-publish/src/views/dss/Main/MAIN_01.vue
+++ dss-publish/src/views/dss/Main/MAIN_01.vue
@@ -1,3 +1,67 @@
 <template>
-
-</template>
+  <div class="main-wrap">
+    <div class="visual-layout-area">
+      <div class="main-visual-wrap">
+        <div class="main-visual-list">
+          <div class="visual-txt-area">
+             <span class="visual-stit">Data + Blue</span>
+            <p class="visual-title">
+              <pan>신한 Data Blue의</pan>
+              <pan>다양한 Data를 </pan>
+              <pan>탐험하세요.</pan>
+            </p>
+          </div>
+          <button href="" class="">Data Blue 바로가기</button>
+          <div class="visual-img"><img src="../../../assets/images/@temp/visual_img1.png" alt="신한 Data Blue의 다양한 Data를 탐험하세요."></div>
+        </div>
+      </div>
+      <div class="my-login-wrap">
+        <div class="outlogin-box login"> 
+          <i class="main-login-photo"></i>
+          <p>로그인 후<br>나만의 맞춤 정보를 이용하세요.</p> 
+          <button>로그인</button>
+        </div>
+        <div class="notice-box">          
+          <div class="notice-banner"><img src=" " alt=""></div>
+          <div class="roll-notice">
+            Trend Report
+            ShinhanCard Data Market 신규 오픈(두줄)
+            2024.08.08
+             
+          </div>
+        </div>       
+      </div>
+    </div>
+    <div class="main-search-wrap">검색</div> 
+    <div class="main-card-wrap">       
+      <button class="card-box">
+        <span class="card-stit">다양한</span>
+        <strong class="card- title">Data 서비스</strong>
+        <i class="ico-main-data"></i>
+        <spoan class="card-txt">Data One-Stop Service</spoan>
+      </button>
+      <button class="card-box">
+        <span class="card-stit">폭발적인</span>
+        <strong class="card- title">솔루션서비스 </strong>
+        <i class="ico-main-solution"></i>
+        <spoan class="card-txt">상권분석, 시장 모니터링</spoan>
+      </button> 
+      <button class="card-box">
+        <span class="card-stit">스마트한</span>
+        <strong class="card-title">광고서비스 </strong>
+        <i class="ico-main-ad "></i>
+        <spoan class="card-txt">최적의 고객 타겟팅</spoan>
+      </button> 
+      <button class="card-box">
+        <span class="card-stit">경이로운</span> 
+        <strong class="card- title">API서비스</strong>
+        <i class="ico-main-api"></i>
+        <spoan class="card-txt">Data 실시간 활용</spoan>
+      </button>    
+    </div>
+    <div class="main-services-wrap">서비스</div>
+    <div class="main-special-wrap">스페셜</div>
+    <div class="main-datable-wrap">데이터블</div>
+    <div class="main-Customer-wrap">고객센터</div>
+  </div> 
+</template> 
