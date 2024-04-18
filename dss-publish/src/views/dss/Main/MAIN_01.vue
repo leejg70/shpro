4a0b650e88e0980ae72e24a4586722784154bdd0
leejg <jeaguen@naver.com>
2024-04-18 17:12:17 +0900

    메인 작업중

--- dss-publish/src/views/dss/Main/MAIN_01.vue
+++ dss-publish/src/views/dss/Main/MAIN_01.vue
@@ -1,8 +1,14 @@
+<script setup>
+// import { Swiper, SwiperSlide } from 'swiper/vue';
+// import 'swiper/swiper.scss';
+
+
+</script>
 <template>
   <div class="main-wrap">
     <div class="visual-layout-area">
       <div class="main-visual-wrap">
-        <div class="main-visual-list">
+        <div class="main-visual-list">  
           <div class="visual-txt-area">
              <span class="visual-stit">Data + Blue</span>
             <p class="visual-title">
@@ -10,9 +16,9 @@
               <pan>다양한 Data를 </pan>
               <pan>탐험하세요.</pan>
             </p>
+            <button href="" class="">Data Blue 바로가기</button>
           </div>
-          <button href="" class="">Data Blue 바로가기</button>
-          <!-- <div class="visual-img"><img src="../../../assets/images/@temp/visual_img1.png" alt="신한 Data Blue의 다양한 Data를 탐험하세요."></div> -->
+          <div class="visual-img"><img src="../../../assets/images/main/visual_img1.png" alt="신한 Data Blue의 다양한 Data를 탐험하세요."></div>
         </div>
       </div>
       <div class="my-login-wrap">
@@ -33,35 +39,64 @@
       </div>
     </div>
     <div class="main-search-wrap">검색</div> 
-    <div class="main-card-wrap">       
-      <button class="card-box">
-        <span class="card-stit">다양한</span>
-        <strong class="card- title">Data 서비스</strong>
-        <i class="ico-main-data"></i>
-        <spoan class="card-txt">Data One-Stop Service</spoan>
-      </button>
-      <button class="card-box">
-        <span class="card-stit">폭발적인</span>
-        <strong class="card- title">솔루션서비스 </strong>
-        <i class="ico-main-solution"></i>
-        <spoan class="card-txt">상권분석, 시장 모니터링</spoan>
-      </button> 
-      <button class="card-box">
-        <span class="card-stit">스마트한</span>
-        <strong class="card-title">광고서비스 </strong>
-        <i class="ico-main-ad "></i>
-        <spoan class="card-txt">최적의 고객 타겟팅</spoan>
-      </button> 
-      <button class="card-box">
-        <span class="card-stit">경이로운</span> 
-        <strong class="card- title">API서비스</strong>
-        <i class="ico-main-api"></i>
-        <spoan class="card-txt">Data 실시간 활용</spoan>
-      </button>    
+    <div class="main-card-wrap">
+      <div class="main-card-inner">
+          <button class="card-box">
+          <span class="card-stit">다양한</span>
+          <strong class="card- title">Data 서비스</strong>
+          <i class="ico-main-data"></i>
+          <spoan class="card-txt">Data One-Stop Service</spoan>
+        </button>
+        <button class="card-box">
+          <span class="card-stit">폭발적인</span>
+          <strong class="card- title">솔루션서비스 </strong>
+          <i class="ico-main-solution"></i>
+          <spoan class="card-txt">상권분석, 시장 모니터링</spoan>
+        </button> 
+        <button class="card-box">
+          <span class="card-stit">스마트한</span>
+          <strong class="card-title">광고서비스 </strong>
+          <i class="ico-main-ad "></i>
+          <spoan class="card-txt">최적의 고객 타겟팅</spoan>
+        </button> 
+        <button class="card-box">
+          <span class="card-stit">경이로운</span> 
+          <strong class="card- title">API서비스</strong>
+          <i class="ico-main-api"></i>
+          <spoan class="card-txt">Data 실시간 활용</spoan>
+        </button>    
+      </div>      
     </div>
-    <div class="main-services-wrap">서비스</div>
-    <div class="main-special-wrap">스페셜</div>
-    <div class="main-datable-wrap">데이터블</div>
-    <div class="main-Customer-wrap">고객센터</div>
+    <div class="main-services-wrap">
+
+      <h2 class="main-title"><span class="fw300">Most Popular</span><span class="fw700">Services</span></h2>
+      <div class="main-services-inner">
+        <button class="services-box"></button>
+        <button class="services-box"></button>
+        <button class="services-box"></button>
+        <button class="services-box"></button>
+        <button class="services-box"></button>
+        <button class="services-box"></button>
+        <button class="services-box"></button>
+        <button class="services-box"></button>
+        <button class="services-box"></button>
+        <div class="main-more-area">
+          <button class="btn-more">더보기</button>
+        </div>
+      </div>
+    
+    
+    </div>
+    <div class="main-special-wrap">
+      <h2 class="main-title"><span class="fw300">For Success</span><span class="fw700">Special Know-how</span></h2>     
+      
+
+    </div>
+    <div class="main-datable-wrap">
+      <h2 class="main-title"><span class="fw300">Value Together</span><span class="fw700">Shinhan Datable</span></h2>  
+    </div>
+    <div class="main-Customer-wrap">
+      <h2 class="main-title"><span class="fw300">Always Open</span><span class="fw700">Customer Center</span></h2>
+    </div>
   </div> 
 </template> 
