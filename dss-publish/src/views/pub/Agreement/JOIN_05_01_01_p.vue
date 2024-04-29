d48c6949e644c8f569e3eeadc08c33214ca3a974
DESKTOP-6KGIGV2\user <72hate@gmail.com>
2024-04-29 18:03:45 +0900

    약관 탭메뉴 작업 중

--- dss-publish/src/views/pub/Agreement/JOIN_05_01_01_p.vue
+++ dss-publish/src/views/pub/Agreement/JOIN_05_01_01_p.vue
@@ -1,22 +1,65 @@
 <script setup>
-const link = 'https://www.shinhancard.com/pconts/html/agree/1217932_7911.html'
+import { ref } from 'vue';
+
+const tab = ref(null);
+const agree = ref(false);
 </script>
 
 <template>
-  <div class="popup-wrap w62">
-    <div class="popup-head">
-      <h3 class="title">휴대폰 본인확인 이용 동의</h3>
+  <v-dialog
+    v-model="agree"
+    width="auto"
+  >
+    <!-- 샘플 버튼 -->
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn v-bind="activatorProps">
+        휴대폰 본인확인 이용 동의 팝업
+      </v-btn>
+    </template>
+    <!-- // 샘플 버튼 -->
+
+    <!-- 휴대폰 본인확인 이용 동의 -->
+    <div class="popup-wrap large">
+      <div class="popup-head">
+        <h3 class="h-title">휴대폰 본인확인 이용 동의</h3>
+      </div>
+      <div class="popup-cont">
+        <v-card>
+          <v-tabs
+            v-model="tab"
+            bg-color="transparent"
+          >
+            <v-tab value="tab01_1">NICE평가정보</v-tab>
+            <v-tab value="tab01_2">KCB</v-tab>
+          </v-tabs>
+          <v-card-text>
+            <v-window v-model="tab">
+              <v-window-item value="tab01_1">
+                제1조 (목적)
+
+본 약관은 NICE평가정보(주) (이하 "회사"라 한다)이 제공하는 본인확인서비스(이하 "서비스"라 한다)에 관한 이용조건 및 절차 등 기본적인 사항을 규정함을 목적으로 합니다.
+              </v-window-item>
+              <v-window-item value="tab01_2">
+                Two
+              </v-window-item>
+            </v-window>
+          </v-card-text>
+        </v-card>
+      </div>
+      <v-btn
+        @click="agree = false"
+        variant="text"
+        class="btn-popup-close"
+      >
+        <span class="sr-only">닫기</span>
+      </v-btn>
     </div>
-    <div class="popup-cont">
-      <iframe :src="link" class="iframe"></iframe>
-    </div>
-  </div>
+    <!-- // 휴대폰 본인확인 이용 동의 -->
+  </v-dialog>
 </template>
 
-<style lang="scss">
-.iframe {
-  border: 1px solid #ddd;
-  width: 100%;
-  height: 25rem;
+<style scoped>
+.v-btn--size-default {
+  font-size: 1.4rem;
 }
 </style>
--- dss-publish/src/views/pub/Agreement/JOIN_05_02_01_01_p.vue
+++ dss-publish/src/views/pub/Agreement/JOIN_05_02_01_01_p.vue
@@ -1,3 +1,63 @@
-<template>
+<script setup>
+import { ref } from 'vue';
 
+const tab = ref(null);
+const agree = ref(false);
+</script>
+
+<template>
+  <v-dialog
+    v-model="agree"
+    width="auto"
+  >
+    <!-- 샘플 버튼 -->
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn v-bind="activatorProps">
+        고유식별정보 처리 동의 팝업
+      </v-btn>
+    </template>
+    <!-- // 샘플 버튼 -->
+
+    <!-- 고유식별정보 처리 동의 -->
+    <div class="popup-wrap large">
+      <div class="popup-head">
+        <h3 class="h-title">고유식별정보 처리 동의</h3>
+      </div>
+      <div class="popup-cont">
+        <v-card>
+          <v-tabs
+            v-model="tab"
+            bg-color="transparent"
+          >
+            <v-tab value="tab01_1">NICE평가정보</v-tab>
+            <v-tab value="tab01_2">KCB</v-tab>
+          </v-tabs>
+          <v-card-text>
+            <v-window v-model="tab">
+              <v-window-item value="tab01_1">
+                One
+              </v-window-item>
+              <v-window-item value="tab01_2">
+                Two
+              </v-window-item>
+            </v-window>
+          </v-card-text>
+        </v-card>
+      </div>
+      <v-btn
+        @click="agree = false"
+        variant="text"
+        class="btn-popup-close"
+      >
+        <span class="sr-only">닫기</span>
+      </v-btn>
+    </div>
+    <!-- // 고유식별정보 처리 동의 -->
+  </v-dialog>
 </template>
+
+<style scoped>
+.v-btn--size-default {
+  font-size: 1.4rem;
+}
+</style>
