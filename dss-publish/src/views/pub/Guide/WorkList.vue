1d738d13bfcd9fb41a59b5fb48239776a3087403
DESKTOP-6KGIGV2\user <72hate@gmail.com>
2024-05-27 17:19:26 +0900

    마이페이지 app key 팝업 완료

--- dss-publish/src/views/pub/Guide/WorkList.vue
+++ dss-publish/src/views/pub/Guide/WorkList.vue
@@ -857,7 +857,7 @@
 									<td class="col-5depth"></td>
 									<td class="col-point">popup</td>
 									<td class="col-url"><router-link to="/MY_03_01_p" target="_blank">/views/pub/My/MY_03_01_p</router-link></td>
-									<td class="col-date"></td>
+									<td class="col-date">2024-05-27</td>
 									<td class="col-complete"></td>
 									<td class="col-memo"></td>
 								</tr>
